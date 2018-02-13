7651a20d60546db2a1fd97626eb6e0e0faa5d356
sunde41 <sunde41@gmail.com>
2018-02-13 10:33:43 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /recoll-webui/
-/usr/local/bin/gunicorn webui-wsgi:application -c /config/.recoll/gunicorn.conf
+/usr/local/bin/gunicorn webui-wsgi:application -c /config/gunicorn.conf
