7bca9aace2237e5e82389fac022af50ee45463bf
sunde41 <sunde41@gmail.com>
2018-02-13 11:23:13 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo bind=0.0.0.0:8080 > /root/gunicorn.conf
+echo bind='0.0.0.0:8080' > /root/gunicorn.conf
 cd /recoll-webui/
 /usr/local/bin/gunicorn webui-wsgi:application -c /root/gunicorn.conf
